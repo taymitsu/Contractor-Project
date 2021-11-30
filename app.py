7a902d422b72d949515316ed539ec28326751efe
Tay <tmitsumori96@gmail.com>
2021-11-29 18:53:23 -0800

    Sign In form added

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', msg='Flask is Cool!')
+    return render_template('index.html')
+
+#HOME
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
-    
