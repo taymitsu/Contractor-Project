99347ff189e94dfecb65cd9308ed144be6a57cba
Tay <tmitsumori96@gmail.com>
2021-11-29 19:08:54 -0800

    Added MongoDB, users and donations

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
- 
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.ContractorProject
+users = db.users
+donations = db.donations
+
+users = [
+    { 'username': 'taymitsu', 'password': 'creamofwheatrox'},
+    { 'username': 'phiggy', 'password': 'Daximoon'}
+]
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#HOME
+#----------------HOME----------------
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+#----------------SHOW----------------
+@app.route('/donations')
+def donation_index():
+    return render_template('donations_index.html', donations=donations.find())
 
 if __name__ == '__main__':
     app.run(debug = True)
