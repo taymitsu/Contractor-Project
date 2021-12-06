f525c3190c2f76f617aab1bab4764b2987902728
Tay <tmitsumori96@gmail.com>
2021-12-06 14:45:25 -0800

    HTML

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import datetime
+import os
 
 client = MongoClient()
 db = client.ContractorProject
